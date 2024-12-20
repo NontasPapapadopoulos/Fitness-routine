305c99ed02e7c4111330279b3acd070446e42427
nondas <papadopoulose92@gmail.com>
2024-12-21 00:58:27 +0200

    add screen to navgraph

--- app/src/main/java/com/example/fitness_routine/presentation/ui/screen/measurements/MeasurementsScreen.kt
+++ app/src/main/java/com/example/fitness_routine/presentation/ui/screen/measurements/MeasurementsScreen.kt
@@ -3,18 +3,30 @@ package com.example.fitness_routine.presentation.ui.screen.measurements
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.SettingsAccessibility
+import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +36,8 @@ import com.example.fitness_routine.domain.entity.BodyMeasurementDomainEntity
 import com.example.fitness_routine.presentation.component.BackButton
 import com.example.fitness_routine.presentation.component.LoadingBox
 import com.example.fitness_routine.presentation.ui.theme.AppTheme
+import com.example.fitness_routine.presentation.ui.theme.contentSpacing2
+import com.example.fitness_routine.presentation.ui.theme.contentSpacing3
 import com.example.fitness_routine.presentation.ui.theme.contentSpacing6
 import com.example.fitness_routine.presentation.util.toFormattedDate
 
@@ -77,22 +91,30 @@ private fun MeasurementsContent(
                 title = {
                     Row(
                         modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        //Text(text = "Date: ${content.bodyMeasurement.date.toFormattedDate()}")
+                        Text(text = "Measurements History")
+
+//                        IconButton(onClick = {  }) {
+//                            Icon(Icons.Outlined.AddCircle, null)
+//                        }
                     }
                 },
                 navigationIcon = { BackButton(navigateBack) }
             )
         }
     ) {
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
-                .padding(contentSpacing6)
+                .padding(horizontal = contentSpacing6)
+                .verticalScroll(rememberScrollState())
         ) {
+
+            NoMeasurementsMessage(isEmpty =
+            content.measurements.isEmpty())
 
             content.measurements.forEach {
                 MeasurementItem(
@@ -101,10 +123,26 @@ private fun MeasurementsContent(
                 )
             }
         }
+
     }
 
 
+}
 
+@Composable
+private fun NoMeasurementsMessage(
+    isEmpty: Boolean
+) {
+    if (isEmpty) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(text = "There are no measurements documented.")
+        }
+
+    }
 }
 
 @Composable
@@ -113,13 +151,43 @@ private fun MeasurementItem(
     onNavigateToMeasurement: () -> Unit
 ) {
 
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onNavigateToMeasurement() }
+            .padding(contentSpacing3)
+            .clickable { onNavigateToMeasurement() },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = measurement.date.toString())
+
+        Text(
+            text = measurement.date.toString()
+        )
+
+        Spacer(modifier = Modifier.width(contentSpacing6))
+
+        Column{
+
+            Text(
+                text = "Weight: ${measurement.weight}"
+            )
+
+            Text(
+                text = "Fat: ${measurement.fat}"
+            )
+
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Icon(
+            Icons.Default.SettingsAccessibility,
+            contentDescription = null
+        )
     }
+
+
 }
 
 @Composable
